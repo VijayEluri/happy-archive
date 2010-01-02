912a1929666aa84c73a0c7483d2cbb268251ed03
Sarah Happy <sarah.a.happy@gmail.com>
2010-01-02 02:05:10 -0400

    remove unused fancy block encoder builder.

---
+++ src/org/yi/happy/archive/BlockEncoderBlobTest.java
@@ -0,0 +1,66 @@
+package org.yi.happy.archive;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.yi.happy.archive.test_data.TestData;
+
+/**
+ * tests for block encode
+ */
+public class BlockEncoderBlobTest {
+    /**
+     * encode a block using default block encoding
+     */
+    @Test
+    public void testContentEncode() {
+	Block in = TestUtil.loadClear();
+
+	BlockEncoder e = new BlockEncoderBlob(DigestFactory.create("sha-256"),
+		CipherFactory.createNamed("rijndael256-256-cbc"));
+	BlockEncoderResult r = e.encode(in);
+	Block out = r.getBlock();
+
+	Block want = TestUtil.loadBlock(TestData.KEY_BLOB);
+	Assert.assertArrayEquals(want.asBytes(), out.asBytes());
+	Assert.assertEquals(TestData.KEY_BLOB.getFullKey(), r.getKey());
+    }
+
+    /**
+     * encode a block with non-default settings
+     */
+    @Test
+    public void testEncodeNonDefault() {
+	Block in = TestUtil.loadClear();
+
+	BlockEncoder be = new BlockEncoderBlob(DigestFactory.create("sha-1"),
+		CipherFactory.createNamed("aes-192-cbc"));
+	BlockEncoderResult r = be.encode(in);
+	Block have = r.getBlock();
+
+	TestData d = TestData.KEY_BLOB_SHA1_AES192;
+	Block want = TestUtil.loadBlock(d);
+	assertEquals(d.getFullKey(), r.getKey());
+	assertArrayEquals(want.asBytes(), have.asBytes());
+    }
+
+    /**
+     * encode with all default settings
+     */
+    @Test
+    public void testEncodeDefault() {
+	Block in = TestUtil.loadClear();
+
+	BlockEncoder be = new BlockEncoderBlob(DigestFactory.create("sha-256"),
+		CipherFactory.createNamed("aes-128-cbc"));
+	BlockEncoderResult r = be.encode(in);
+	Block have = r.getBlock();
+
+	TestData d = TestData.KEY_BLOB_AES128;
+	Block want = TestUtil.loadBlock(d);
+	assertEquals(d.getFullKey(), r.getKey());
+	assertArrayEquals(want.asBytes(), have.asBytes());
+    }
+}
---
+++ src/org/yi/happy/archive/CipherFactoryTest.java
@@ -0,0 +1,14 @@
+package org.yi.happy.archive;
+
+import org.junit.Test;
+
+
+public class CipherFactoryTest {
+    /**
+     * setting a bad cipher should fail right away
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testBadCipher() {
+	CipherFactory.createNamed("bad");
+    }
+}
