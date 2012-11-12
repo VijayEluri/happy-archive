f77a86b4b3b1e2d46700793840403129ba946f92
Sarah Happy <sarah.a.happy@gmail.com>
2012-11-12 10:19:45 -0400

    restrict the command line annotations so they can only be applied to types.

--- src/org/yi/happy/archive/commandLine/UsesStore.java
+++ src/org/yi/happy/archive/commandLine/UsesStore.java
@@ -1,9 +1,12 @@
 package org.yi.happy.archive.commandLine;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface UsesStore {
 
 }
