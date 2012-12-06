46761ab1ccf6bc124738e779c99a4806df3c5d74
Sarah Happy <sarah.a.happy@gmail.com>
2012-12-06 15:44:26 -0400

    move all the output handling logic into the callback.

--- src/org/yi/happy/archive/sandbox/interpret/InterpretFilter.java
+++ src/org/yi/happy/archive/sandbox/interpret/InterpretFilter.java
@@ -8,33 +8,107 @@ import org.yi.happy.archive.tag.BinaryHandler;
  */
 public class InterpretFilter implements BinaryHandler {
 
-    private final BinaryHandler handler;
-    private final ActionCallback callback = new ActionCallback() {
+    private class OutputHandler implements ActionCallback {
+        private final BinaryHandler handler;
+
+        public OutputHandler(BinaryHandler handler) {
+            this.handler = handler;
+        }
+
         @Override
         public void startStream() {
-            sendStartStream();
+            flush();
+            handler.startStream();
         }
 
         @Override
         public void endStream() {
-            sendEndStream();
-        }
-
-        @Override
-        public void consumeAndSend() {
-            sendData();
+            flush();
+            handler.endStream();
         }
 
         @Override
         public void startRegion(String name) {
-            sendStartRegion(name);
+            flush();
+            handler.startRegion(name);
         }
 
         @Override
         public void endRegion(String name) {
-            sendEndRegion(name);
+            flush();
+            handler.endRegion(name);
         }
-    };
+
+        /*
+         * support for processing the bytes individually but sending them in
+         * blocks when possible.
+         * 
+         * Interacts tightly with InterpretFilter.bytes(byte[], int, int).
+         */
+
+        private byte[] sendBuff;
+        private int sendStart;
+        private int sendCurrent;
+        private int sendEnd;
+        private int sendLimit;
+
+        public void flush() {
+            if (sendBuff == null) {
+                return;
+            }
+
+            if (sendStart != sendEnd) {
+                handler.bytes(sendBuff, sendStart, sendEnd - sendStart);
+            }
+
+            sendStart = sendCurrent;
+            sendEnd = sendCurrent;
+        }
+
+        @Override
+        public void consumeAndSend() {
+            if (sendBuff == null) {
+                throw new UnsupportedOperationException();
+            }
+
+            if (sendCurrent != sendEnd) {
+                flush();
+            }
+
+            if (sendCurrent >= sendLimit) {
+                throw new IndexOutOfBoundsException();
+            }
+
+            sendEnd++;
+            sendCurrent++;
+        }
+
+        public void startBytes(byte[] buff, int offset, int length) {
+            sendStart = offset;
+            sendCurrent = offset;
+            sendEnd = offset;
+            sendLimit = offset + length;
+            sendBuff = buff;
+        }
+
+        public boolean hasCurrentByte() {
+            return sendCurrent < sendLimit;
+        }
+
+        public byte getCurrentByte() {
+            return sendBuff[sendCurrent];
+        }
+
+        public void endBytes() {
+            try {
+                flush();
+            } finally {
+                sendBuff = null;
+            }
+        }
+    }
+
+    private OutputHandler callback;
 
     private RuleState state;
 
@@ -48,58 +122,8 @@ public class InterpretFilter implements BinaryHandler {
      *            the output handler.
      */
     public InterpretFilter(RuleState state, BinaryHandler handler) {
-        this.handler = handler;
+        this.callback = new OutputHandler(handler);
         this.state = state;
-    }
-
-    protected void sendEndRegion(String name) {
-        flush();
-        handler.endRegion(name);
-    }
-
-    protected void sendStartRegion(String name) {
-        flush();
-        handler.startRegion(name);
-    }
-
-    private byte[] sendBuff;
-    private int sendStart;
-    private int sendCurrent;
-    private int sendEnd;
-
-    protected void sendData() {
-        if (sendBuff == null) {
-            throw new UnsupportedOperationException();
-        }
-
-        if (sendCurrent != sendEnd) {
-            flush();
-        }
-
-        sendEnd++;
-        sendCurrent++;
-    }
-
-    private void flush() {
-        if (sendBuff == null) {
-            return;
-        }
-
-        if (sendStart != sendEnd) {
-            handler.bytes(sendBuff, sendStart, sendEnd - sendStart);
-        }
-        sendStart = sendCurrent;
-        sendEnd = sendCurrent;
-    }
-
-    protected void sendEndStream() {
-        flush();
-        handler.endStream();
-    }
-
-    protected void sendStartStream() {
-        flush();
-        handler.startStream();
     }
 
     @Override
@@ -114,18 +138,13 @@ public class InterpretFilter implements BinaryHandler {
 
     @Override
     public void bytes(byte[] buff, int offset, int length) {
-        int end = offset + length;
-        sendStart = offset;
-        sendCurrent = offset;
-        sendEnd = offset;
-        sendBuff = buff;
+        callback.startBytes(buff, offset, length);
         try {
-            while (sendCurrent < end) {
-                state = state.data(buff[sendCurrent], callback);
+            while (callback.hasCurrentByte()) {
+                state = state.data(callback.getCurrentByte(), callback);
             }
-            flush();
         } finally {
-            sendBuff = null;
+            callback.endBytes();
         }
     }
 
