7eacd214e2d0f67f60e90d0f222631030adc42a2
Sarah Happy <sarah.a.happy@gmail.com>
2013-10-28 09:45:11 -0300

    stop using RestoreFile

---
+++ src/org/yi/happy/annotate/RestoreLoop.java
@@ -0,0 +1,12 @@
+package org.yi.happy.annotate;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Mark a restore loop.
+ */
+@Retention(RetentionPolicy.SOURCE)
+public @interface RestoreLoop {
+
+}
--- src/org/yi/happy/archive/FileStoreFileGetMain.java
+++ src/org/yi/happy/archive/FileStoreFileGetMain.java
@@ -4,14 +4,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.yi.happy.annotate.RestoreLoop;
+import org.yi.happy.archive.block.Block;
 import org.yi.happy.archive.commandLine.UsesArgs;
 import org.yi.happy.archive.commandLine.UsesNeed;
 import org.yi.happy.archive.commandLine.UsesStore;
 import org.yi.happy.archive.file_system.FileSystem;
+import org.yi.happy.archive.file_system.RandomOutputFile;
 import org.yi.happy.archive.key.FullKey;
 import org.yi.happy.archive.key.FullKeyParse;
 import org.yi.happy.archive.key.LocatorKey;
-import org.yi.happy.archive.tag.RestoreFile;
+import org.yi.happy.archive.restore.RestoreEngine;
 
 /**
  * get a file from a file store.
@@ -58,31 +61,61 @@ public class FileStoreFileGetMain implements MainCommand {
      * @throws IOException
      */
     @Override
+    @RestoreLoop
     public void run() throws IOException {
         FullKey key = FullKeyParse.parseFullKey(args.get(0));
         String path = args.get(1);
 
-        RestoreFile r = new RestoreFile(key, new StorageClearBlockSource(store),
-                path, fs);
-        r.step();
-        while (!r.isDone()) {
-            notReady(r);
-            r.step();
+        ClearBlockSource source = new StorageClearBlockSource(store);
+        RestoreEngine engine = new RestoreEngine(key);
+
+        /*
+         * do the work
+         */
+        while (true) {
+            boolean progress = false;
+            RandomOutputFile out = null;
+            try {
+                engine.start();
+                while (engine.findReady()) {
+                    Block block = source.get(engine.getKey());
+                    if (block == null) {
+                        engine.skip();
+                        continue;
+                    }
+                    Fragment part = engine.step(block);
+                    progress = true;
+                    if (part != null) {
+                        if (out == null) {
+                            out = fs.openRandomOutputFile(path);
+                        }
+                        out.writeAt(part.getOffset(), part.getData()
+                                .toByteArray());
+                    }
+                }
+            } finally {
+                if (out != null) {
+                    out.close();
+                    out = null;
+                }
+            }
+            if (engine.isDone()) {
+                break;
+            }
+            notReady(engine, progress);
         }
     }
 
-    private void notReady(RestoreFile reader) throws IOException {
+    private void notReady(RestoreEngine engine, boolean progress)
+            throws IOException {
         List<LocatorKey> keys = new ArrayList<LocatorKey>();
-        for (FullKey key : reader.getPending()) {
+        for (FullKey key : engine.getNeeded()) {
             keys.add(key.toLocatorKey());
         }
         needHandler.post(keys);
 
-        waitHandler.doWait(progress != reader.getProgress());
-        progress = reader.getProgress();
+        waitHandler.doWait(progress);
     }
-
-    private int progress;
 
     private WaitHandler waitHandler;
 }
